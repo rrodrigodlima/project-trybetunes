fe7d503a599e5e8f314cf1e0bffd07a9d5baff94
rrodrigodlima <rdrigo019@gmail.com>
2023-01-16 12:30:40 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchInput: '',
+    isDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ searchInput: value });
+
+    if (value.length > 1) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { searchInput, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+
+        <form>
+          <input
+            type="text"
+            placeholder="Search artist or band"
+            data-testid="search-artist-input"
+            value={ searchInput }
+            onChange={ this.onInputChange }
+          />
+
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Search
+          </button>
+
+        </form>
+
       </div>
     );
   }
