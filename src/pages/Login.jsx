8953b34238c980a6113792632d7db6c43cb3062a
rrodrigodlima <rdrigo019@gmail.com>
2023-01-16 11:19:57 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    btnDisabled: true,
+    isLoading: false,
+    redirect: false,
+    inputName: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState({ inputName: value });
+
+    if (value.length > 2) {
+      this.setState({
+        btnDisabled: false,
+      });
+    } else {
+      this.setState({
+        btnDisabled: true,
+      });
+    }
+  };
+
+  onBtnClick = async () => {
+    const { inputName } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: inputName });
+    this.setState({ redirect: true });
+  };
+
   render() {
+    const { btnDisabled, isLoading, redirect } = this.state;
+    if (isLoading) {
+      return (
+        <div>
+          <p>Carregando...</p>
+          { redirect && <Redirect to="/search" /> }
+        </div>
+      );
+    }
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            placeholder="Digite seu nome"
+            onChange={ this.onInputChange }
+          />
+
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            onClick={ this.onBtnClick }
+            disabled={ btnDisabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
